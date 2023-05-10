1015b4ca3da78793ec807313139df80f061c03a3
lazzervan <84437576+lazzervan@users.noreply.github.com>
2023-05-10 17:32:15 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,183 +1,79 @@
 <!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Tutorshop Admin Panel</title>
-
-</head>
-
-<body>
-    <div class="side-menu">
-        <div class="brand-name">
-            <img src="img/logo-white.png" alt="" width="140px">
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Admin LogIn</title>
+        <link rel="stylesheet" href="css/adminlogin.css">
+    </head>
+    <body>
+        <div class="center">
+            <h1>Tutorshop Admin Login</h1>
+            <form action="index.html" method="POST">
+                <div class="txt_field">
+                    <input type="text" name="username" required>
+                    <span></span>
+                    <label for="username">Username</label>
+                </div>
+                <div class="txt_field">
+                    <input type="password" name="password" required>
+                    <span></span>
+                    <label for="password">Password</label>
+                </div>
+                <div class="pass">Forgot Password?</div>
+                <button id="log-in">Login</button>
+                <div class="userlink">
+                    Not an Admin? <a href="https://tutorshopph.herokuapp.com/">Redirect to User Site</a>
+                </div>
+            </form>
         </div>
-        <ul>
-            <a href="index.html">
-                <li><span>Dashboard</span></li>
-            </a>
-            <a href="students.html">
-                <li><span>Students</span></li>
-            </a>
-            <a href="tutors.html">
-                <li><span>Tutors</span></li>
-            </a>
-            <a href="income.html">
-                <li><span>Income</span></li>
-            </a>
-            <a href="messages.html">
-                <li><span>Messages</span></li>
-            </a>
-            <a href="transactions.html">
-                <li><span>Transactions</span></li>
-            </a>
-        </ul>
-    </div>
-    <div class="container">
-        <div class="header">
-            <div class="nav">
-                <div class="search">
-                    <input type="text" placeholder="Search...">
-                    <button type="submit"><img src="icon/search.png" alt="" ></button>
-                </div>
-                <div class="user">
-                    <a href="#" class="btn">Add New</a>
-                    <img src="icon/smile.png" alt="">
-                    <div class="img-case">
-                        <img src="icon/smile.png" alt="">
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="content">
-            <div class="cards">
-                <div class="card">
-                    <div class="box">
-                        <h3>2194 Students</h3>
-                        <h3>530 Teachers</h3>
-                        <h3>112 Subjects</h3>
-                    </div>
-                    <div class="icon-case">
-                        <img src="icon/smile.png" alt="">
-                    </div>
-                </div>
-                <div class="card">
-                    <div class="box">
-                        <h1>Php 33,095.00</h1>
-                        <h3>Income</h3>
-                    </div>
-                   
-                </div>
-                <div class="card">
-                    <img src="image/graph1.png" style="max-width: 100%; max-height: 100%;" alt="">
-                </div>
-                <div class="card">
-                    <img src="image/graph2.png" style="max-width: 100%; max-height: 100%;" alt="">
-                </div>
-            </div>
-            <div class="content-2">
-                <div class="recent-payments">
-                    <div class="title">
-                        <h2>Recent Payments</h2>
-                        <a href="#" class="btn">View All</a>
-                    </div>
-                    <table>
-                        <tr>
-                            <th>Name</th>
-                            <th>School</th>
-                            <th>Amount</th>
-                            <th>Option</th>
-                        </tr>
-                        <tr>
-                            <td>Mercia Zeb</td>
-                            <td>WMSU</td>
-                            <td>Php 199</td>
-                            <td>
-                                <a href="#" class="btn">View</a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Katharyn Clarence</td>
-                            <td>WMSU</td>
-                            <td>Php 199</td>
-                            <td>
-                                <a href="#" class="btn">View</a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Laila Junior</td>
-                            <td>WMSU</td>
-                            <td>Php 199</td>
-                            <td>
-                                <a href="#" class="btn">View</a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Em Shaye</td>
-                            <td>WMSU</td>
-                            <td>Php 199</td>
-                            <td>
-                                <a href="#" class="btn">View</a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Lacie Docia</td>
-                            <td>WMSU</td>
-                            <td>Php 199</td>
-                            <td>
-                                <a href="#" class="btn">View</a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Adrian Gay</td>
-                            <td>WMSU</td>
-                            <td>Php 199</td>
-                            <td>
-                                <a href="#" class="btn">View</a>
-                            </td>
-                        </tr>
-                    </table>
-                </div>
-                <div class="new-students">
-                    <div class="title">
-                        <h2>New Students</h2>
-                        <a href="#" class="btn">View All</a>
-                    </div>
-                    <table>
-                        <tr>
-                            <th>Profile</th>
-                            <th>Name</th>
-                            <th>Option</th>
-                        </tr>
-                        <tr>
-                            <td>
-                                <img src="icon/smile.png" alt="">
-                            </td>
-                            <td>Student 2</td>
-                            <td><img src="icon/smile.png" alt=""></td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <img src="icon/smile.png" alt="">
-                            </td>
-                            <td>Student 2</td>
-                            <td><img src="icon/smile.png" alt=""></td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <img src="icon/smile.png" alt="">
-                            </td>
-                            <td>Student 2</td>
-                            <td><img src="icon/smile.png" alt=""></td>
-                        </tr>
-                    </table>
-                </div>
-            </div>
-        </div>
-    </div>
-</body>
+    </body>
 
 </html>
+<script type="text/javascript">
+    window.onload = () => {
+        this.sessionStorage.setItem("username", "admin");
+        this.sessionStorage.setItem("password", "admin");
+    }
+    var input = document.getElementsByTagName('input');
+    var login = document.getElementById('log-in');
+    var form = document.querySelector('form');
+    form.onsubmit = ()=> { return false }
+    login.onclick = ()=> {
+        if ((input[0].value != "") && (input[1].value != "")) {
+            if ((input[0].value == sessionStorage.getItem("username")) && (input[1].value == sessionStorage.getItem("password"))) {
+                form.onsubmit = () => { return 1 }
+                document.cookie = "username" + input[0].value;
+                document.cookie = "password" + input[1].value;
+
+            }
+            else {
+                if ((input[0].value != sessionStorage.getItem("username"))) {
+                    input[0].nextElementSibling.textContent = "Username Not Match";
+                    setTimeout(() => {
+                        input[0].nextElementSibling.textContent = "";
+                    }, 2000);
+                }
+                if ((input[1].value != sessionStorage.getItem("username"))) {
+                    input[1].nextElementSibling.textContent = "Password Not Match";
+                    setTimeout(() => {
+                        input[1].nextElementSibling.textContent = "";
+                    }, 2000);
+                }
+            }
+        }
+        else {
+            if (input[0].value == "") {
+                input[0].nextElementSibling.textContent = "Username is empty";
+                setTimeout(() => {
+                    input[0].nextElementSibling.textContent = "";
+                }, 2000);
+            }
+            if (input[1].value == "") {
+                input[1].nextElementSibling.textContent = "Password is empty";
+                setTimeout(() => {
+                    input[1].nextElementSibling.textContent = "";
+                }, 2000);
+            }
+        }
+    }
+</script>
